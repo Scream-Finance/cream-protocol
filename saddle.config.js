45869067c2ca7cba46f5556d2fa266c3f571c264
Jeremy Yang <jeremy@cream.finance>
2021-11-22 18:22:54 +0800

    update gas price on bsc

--- saddle.config.js
+++ saddle.config.js
@@ -223,7 +223,7 @@ module.exports = {
         ],
         gas_price: [
           {env: "GAS_PRICE"},
-          {default: "12000000000"}
+          {default: "5100000000"}
         ],
         options: {
           transactionConfirmationBlocks: 1,
